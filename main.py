84aa4eca5667fa103dbbe9b1bd01f889a08df3a7
Bogdan <bogdanbbr2000@gmail.com>
2025-03-16 15:18:13 +0200

    Test

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from telebot import TeleBot
 bot = TeleBot('7784381573:AAHKYDRFrFqK7g5qbYn_BiEBk9qGWS_g3nA')
 @bot.message_handler(commands=['start'])
 def start(message):
-    bot.send_message(message.chat.id, 'TEst')
+    bot.send_message(message.chat.id, 'Привет Аня')
     
     
     
